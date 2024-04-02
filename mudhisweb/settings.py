24868baeca6624f2715b34b76fbcd2fbf28a498d
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-04-02 17:28:16 +0300

    Nothing

--- mudhisweb/settings.py
+++ mudhisweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n=fh)gs*ofb6uw#1j!$n=0gcmsjf4szds^--69(fr9pyo*3_j9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['MustafaGulsoy.pythonanywhere.com']
 
 
 # Application definition
