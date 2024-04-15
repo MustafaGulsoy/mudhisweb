db71e4a42afc3306956b550bf24ef5de3a19da02
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-04-15 17:13:40 +0300

    done

--- mudhisweb/settings.py
+++ mudhisweb/settings.py
@@ -113,6 +113,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+STATIC_ROOT = os.path.join(BASE_DIR,"staticFiles")
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
